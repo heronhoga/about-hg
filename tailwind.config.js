b0287dd8231d56c9915bfd37951e1693813439ed
heronhoga <nozzeravixer@gmail.com>
2025-03-30 11:10:53 +0700

    demonic dice

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,19 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {},
-  },
+    theme: {
+      extend: {
+        keyframes: {
+          gradient: {
+            '0%': { backgroundPosition: '0% 50%' },
+            '50%': { backgroundPosition: '100% 50%' },
+            '100%': { backgroundPosition: '0% 50%' },
+          },
+        },
+        animation: {
+          gradient: 'gradient 8s linear infinite'
+        },
+      },
+    },
   plugins: [],
 }
